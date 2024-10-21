535dc391d4823f813f37f8b1363439ebe720fbe4
bhuved <bhuvedu@gmail.com>
2024-10-21 10:45:42 -0400

    updated code for validation and added comments

--- Program.cs
+++ Program.cs
@@ -1,22 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Prompt the user to enter name
+using System.Linq.Expressions;
+
 Console.WriteLine("Enter your name");
 string name = Console.ReadLine();
 
+//Loop continues till the user wants to continue
 bool cont = true;
 while (cont)
 {
     //Prompt the user to enter number
+    repeat:
     Console.WriteLine($"Hi {name}, Enter a number between 1 and 100");
+    //checking for valid number
+    string numString = Console.ReadLine();
+    //handle exception if user enters string value
+    try { int checkValid = Convert.ToInt32(numString); }
+    catch (FormatException)
+    {
+        Console.WriteLine("{0} is not an integer", numString);
+        goto repeat;
+    }
+    int numberEntered = Convert.ToInt32(numString);
 
-    int numberEntered = Convert.ToInt32(Console.ReadLine());
+
     if (numberEntered <= 0 || numberEntered > 100)
     {
-        Console.WriteLine("Enter Valid number");
-
+        Console.WriteLine("InValid number");
+        goto repeat;
     }
     else
     {
+        // checking the num is odd and print the message accordingly
         int numCheck = numberEntered % 2;
         if (numCheck != 0)
         {
@@ -28,7 +43,7 @@ while (cont)
             {
                 Console.WriteLine(numberEntered + " is Odd and greater then 60");
             }
-        }
+        } // checking the num is even and print the message accordingly
         else if (numCheck == 0)
         {
             if (numberEntered >= 2 && numberEntered < 25)
@@ -45,7 +60,7 @@ while (cont)
             }
 
         }
-    }
+    }// prompt the user wants to continue
     Console.WriteLine(name +", Do you want to stop y/n");
     string answer = Console.ReadLine();
     if (answer != null)
@@ -58,6 +73,9 @@ while (cont)
                 break;
             case "y":
             case "yes":
+                cont = false;
+                break;
+            default:
                 cont = false;
                 break;
 
--- obj/Debug/net8.0/NumberAnalyzer.AssemblyInfo.cs
+++ obj/Debug/net8.0/NumberAnalyzer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberAnalyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed29dfd0135b788c2ea242abbb420f29fa4fda24")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberAnalyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberAnalyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
